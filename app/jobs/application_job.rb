7498a13e1eada98e108368e75ce9019833b425cc
Manuel CARRILHO <manuel.carrilho@ntn-europe.fr>
2024-02-05 16:49:18 +0100

    Lundi_05_02-avant-de-partir

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,11 +1,11 @@
 class ExampleJob < ApplicationJob
+include NAP::
   queue_as :default
 
   def perform(*args)
     # Simulates a long, time-consuming task
-    sleep 5
-    # Will display current time, milliseconds included
-    p "hello from ExampleJob #{Time.now().strftime('%F - %H:%M:%S.%L')}"
+    puts "I am #{name}, running my first job at #{age}"
+    #any other valid Ruby/Rails code goes here!
   end
 
 end
